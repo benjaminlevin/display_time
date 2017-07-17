37e596273ab8983d53d9c7b987aeb797967c0b5c
Ben Levin <benjaminwlevin@gmail.com>
2017-07-17 15:28:32 -0500

    first commit, chicago time zone

--- apps/time1/views.py
+++ apps/time1/views.py
@@ -1,17 +1,23 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.shortcuts import render, HttpResponse
 from django.shortcuts import render, redirect
 from django.utils.timezone import get_current_timezone
 from django.utils import timezone
 from datetime import datetime
 
 def index(request):
-    request.date = datetime.now().strftime('%b %d, %Y')
-    request.time = datetime.now().strftime('%-I:%M %p')
+    date = datetime.now().strftime('%b %d, %Y')
+    time = datetime.now().strftime('%-I:%M %p')
+    context = { 
+    	'datetime' : [
+  			{'date' : date},
+  			{'time' : time}
+  		]
+  	}
     #how to set time zone to local for end user and fixed for db?
     print ("*"*50)
     print get_current_timezone()
     print datetime.now().strftime('%-I:%M %p')
     print ("*"*50)
-    return render(request, "time1/index.html")
-
+    return render(request, "time1/index.html", context)
